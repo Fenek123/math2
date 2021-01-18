17efcab8d8d2236bf34c7a8e51c276f6040645da
Fenek123 <nik_lunevF4771@bk.ru>
2021-01-18 11:12:37 +0700

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,26 @@
+fun main()
+{
+    var a = readLine()!!.toDouble()
+    var b = readLine()!!.toDouble()
+    var p = 3.14
+    var A = a * a
+    var B = b * b
+    var S1 = p * A
+    var S2 = p * B
+    var S3 = S2 - S1
+    println(S1)
+    println(S2)
+    println(S3)
+
+
+
+
+
+
+
+
+
+
+
+
+}
